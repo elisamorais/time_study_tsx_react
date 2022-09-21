f109d0d545422ebea97b170b3b911192c216bcbe
Elisa Morais <elisa.morais74@gmail.com>
2022-09-21 17:34:35 -0300

    A3. Criando um componente(Lista)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Formulario from './components/Formulario.tsx';
+import Lista from './components/Lista';
+
 
 function App() {
   return (
     <div className="App">
-      <Formulario />
+      <Lista />
     </div>
   );
 }
