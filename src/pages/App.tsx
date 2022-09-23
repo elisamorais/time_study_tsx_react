065abadb4ba1edb53fb199c96782b00c5a4c27e6
Elisa Morais <elisa.morais74@gmail.com>
2022-09-23 17:40:12 -0300

    B2. CSS (estilizando com Sass)

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Formulario from '../components/Formulario.tsx';
+import Lista from '../components/Lista';
+import './style.scss';
+
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
