7e17e2b00adb6c6dcdcfb0cf08663da5e918e132
Elisa Morais <elisa.morais74@gmail.com>
2022-09-23 21:02:17 -0300

    C3. Pros (Criando o cronômetro)

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import style from './Botao.module.scss';
 
 
-class Botao extends React.Component <{children: any}> {
+class Botao extends React.Component<{children: any}> {
     render() {
         return (
             <button className={style.botao}>
